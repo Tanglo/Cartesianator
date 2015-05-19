84ada57190bef62a9f39167c200ffe0fcde061cc
Tanglo <Tanglo@users.noreply.github.com>
2015-05-19 16:05:18 +1000

    Initial Commit

---
+++ Cartesianator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Cartesianator
+//
+//  Created by Lee Walsh on 19/05/2015.
+//  Copyright (c) 2015 Lee David Walsh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
